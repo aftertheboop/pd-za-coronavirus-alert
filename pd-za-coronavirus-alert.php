aef0887ea7b543b5c53cbf867ff6a9bbe5a17867
Rory Molyneux <rorymolyneux@gmail.com>
2020-03-29 14:35:51 +0200

    Initial plugin commit

---
+++ pd-za-coronavirus-alert.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: PD ZA Coronavirus Alert
+ * Description: Displays an alert at the top of the page linking to relevant Coronavirus resources
+ * Version: 1.0
+ * Author: Rory M
+ *
+ * Text Domain: pd-za-coronavirus-alert
+ *
+ * @package PD-ZA-Coronavirus-Alert
+ */
+
+ 
